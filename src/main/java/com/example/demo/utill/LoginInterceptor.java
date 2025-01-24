ea2d6381df7d357a62a73b2ded0d90c0f84f3d93
fqsic <1473784367@qq.com>
2025-01-24 23:00:08 +0800

    添加登录拦截器

---
+++ src/main/java/com/example/demo/utill/LoginInterceptor.java
@@ -0,0 +1,20 @@
+package com.example.demo.utill;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		if (session.getAttribute("Syokui") == null) {
+			response.sendRedirect("/login");
+			return false;
+		}
+		return true;
+	}
+}
